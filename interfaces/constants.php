ef66930a5649ae4af001c5858a206b44f961e563
sssteveCA <steppuggio@gmail.com>
2022-02-10 22:40:40 +0100

    Created menu in control panel

--- interfaces/constants.php
+++ interfaces/constants.php
@@ -4,6 +4,13 @@ interface Constants{
     //Name of database table(without prefix)
     const TABLE_NAME = "meta_tag_editor";
     const LOG_FILE = ABSPATH."/log.txt";
+    
+    //menu
+    const MENU_TITLE = "Modifica meta tag";
+    const MENU_PAGE_TITLE = "Meta tag editor";
+    const MENU_CAPABILITY = "administrator";
+    const MENU_SLUG = 'meta-tag-editor';
+    const MENU_POSITION = 1;
 
 }
 ?>
--- meta-tag-editor.php
+++ meta-tag-editor.php
@@ -11,7 +11,11 @@
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  */
 
+use Constants as C;
+use Html as H;
+
 require_once('interfaces/constants.php');
+require_once('interfaces/html.php');
 require_once(ABSPATH.'wp-admin/includes/upgrade.php');
 
 //When plugin is activated
@@ -19,8 +23,8 @@ register_activation_hook(__FILE__, 'mte_activator');
 
 function mte_activator(){
     global $wpdb;
-    file_put_contents(Constants::LOG_FILE,"mte_activator\r\n",FILE_APPEND);
-    $table_name = $wpdb->prefix.Constants::TABLE_NAME;
+    file_put_contents(C::LOG_FILE,"mte_activator\r\n",FILE_APPEND);
+    $table_name = $wpdb->prefix.C::TABLE_NAME;
     $sql = <<<SQL
     CREATE TABLE `{$table_name}` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,5 +37,18 @@ function mte_activator(){
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 SQL;
     dbDelta($sql);
+}
+
+//Print the menu in control panel
+add_action('admin_menu','mte_menu');
+
+function mte_menu(){
+    add_menu_page(C::MENU_PAGE_TITLE,C::MENU_TITLE,C::MENU_CAPABILITY,C::MENU_SLUG,'mte_main_menu','',C::MENU_POSITION);
+}
+
+//HTML for main menu page
+function mte_main_menu(){
+    //file_put_contents(C::LOG_FILE,H::MAIN_MENU."\r\n",FILE_APPEND);
+    echo H::MAIN_MENU;
 }
 ?>
