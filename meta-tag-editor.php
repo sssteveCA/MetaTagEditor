c1e1bdd21ca30204fe418627dd8593d84b97f006
sssteveCA <steppuggio@gmail.com>
2022-02-10 21:47:09 +0100

    Meta tag Editor Wordpress plugin started

---
+++ meta-tag-editor.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Meta tag editor
+ * Description: This plugin allow to modify meta tag of wordpress pages
+ * Version: 1.0
+ * Requires at least: 5.0
+ * Requires PHP: 7.2
+ * Author: Stefano Puggioni
+ * Author URI: https://github.com/sssteveCA
+ * License: GPL v3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+?>
